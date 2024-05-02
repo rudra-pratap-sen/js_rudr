09c35ac0dc4419dac0155877dd806a0b5ae7cd35
RudraPratapSen <165404213+rudra-pratap-sen@users.noreply.github.com>
2024-05-02 11:20:22 +0000

    dates

--- 01_basic/08_dates.js
+++ 01_basic/08_dates.js
@@ -6,10 +6,10 @@ let myDate = new Date()
 // console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23) // January start from zero
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
+// let myCreatedDate = new Date("01-14-2023")
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
@@ -19,9 +19,9 @@ let myTimeStamp = Date.now()
 // console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1); +1 bcs of jan starting from zero
+// console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 
